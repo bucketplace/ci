3af5d91e23edba6b80c1a292401e6c1c0e8e5eb6
Jisoo Moon <js.moon@bucketplace.net>
2023-11-22 15:51:40 +0900

    add node javascript file

--- .github/actions/setup-mortar/index.js
+++ .github/actions/setup-mortar/index.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const { getDownloadObject } = require('./lib/utils');
 
 async function setup() {
     try {
@@ -9,15 +8,18 @@ async function setup() {
         const version = core.getInput('version');
 
         // Download the specific version of the tool, e.g. as a tarball/zipball
-        const download = getDownloadObject(version);
-        const pathToTarball = await tc.downloadTool(download.url);
+        // const download = getDownloadObject(version); //TODO: mocking the download object for now
+        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.1.0/mortar_Linux_x86_64.tar.gz');
 
-        // Extract the tarball/zipball onto host runner
-        const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
-        const pathToCLI = await extract(pathToTarball);
+        // // Extract the tarball/zipball onto host runner
+        // // const extract = download.url.endsWith('.tar.gz') ? tc.extractZip : tc.extractTar;
+        // const pathToCLI = await extract(pathToTarball);
+
+        // Extract the tarball onto the runner
+        const pathToCLI = await tc.extractTar(pathToTarball);
 
         // Expose the tool by adding it to the PATH
-        core.addPath(path.join(pathToCLI, download.binPath));
+        core.addPath(pathToCLI)
     } catch (e) {
         core.setFailed(e);
     }
