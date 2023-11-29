68ab4d3588185deb597f0a723d44c71d689fe5d2
Jisoo Moon <js.moon@bucketplace.net>
2023-11-29 22:24:29 +0900

    update mortar v2 patch version

--- .github/actions/setup-mortar/index.js
+++ .github/actions/setup-mortar/index.js
@@ -6,7 +6,7 @@ async function setup() {
 
     try {
         // Download the specific version of the tool, e.g. as a tarball/zipball
-        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.1.0/mortar_Linux_x86_64.tar.gz');
+        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.1.2/mortar_Linux_x86_64.tar.gz');
 
         // Extract the tarball onto the runner
         const pathToCLI = await tc.extractTar(pathToTarball);
