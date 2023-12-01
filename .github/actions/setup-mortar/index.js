b25b21b6b79544e044ac75d59b7fb7cfa3d2e997
Jisoo Moon <98301649+jsmoonB@users.noreply.github.com>
2023-12-01 15:29:40 +0900

    [COREPL-1361] Add mortar setup flow (ignore-stats) (#50)
    ## Proposed Changes
    * TESTING and add ci workflow for setting up mortar step
    
    ## Test Status
    (테스트를 어떻게 진행했는지 설명합니다.)

---
+++ .github/actions/setup-mortar/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function setup() {
+
+    try {
+        // Download the specific version of the tool, e.g. as a tarball/zipball
+        // TODO: https://ohouse.atlassian.net/browse/COREPL-1482 get latest version
+        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.1.3/mortar_Linux_x86_64.tar.gz');
+
+        // Extract the tarball onto the runner
+        const pathToCLI = await tc.extractTar(pathToTarball);
+
+        // Expose the tool by adding it to the PATH
+        const path = core.addPath(pathToCLI)
+        console.log(`Added ${pathToCLI} to PATH: ${path}`);
+    } catch (e) {
+        core.setFailed(e);
+    }
+}
+
+module.exports = setup
+
+if (require.main === module) {
+    setup();
+}
