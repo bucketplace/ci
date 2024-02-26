3033d460e3c823bd0babf135c0a8533480d8a142
Jisoo Moon <98301649+jsmoonB@users.noreply.github.com>
2024-02-26 23:11:40 +0900

    [COREPL-1611] Fix Mortar multi-module (#65)
    ## Proposed Changes
    * https://github.com/bucketplace/mortar/pull/118 <- fix multi-module 권한
    관련해서 추후에 cli cmd 의 디버깅 및 확인을 위해서 버전 업데이트 진행 합니다.
    * 참고로 아직 해당 버전 릴리즈는 안했지만 추후 릴리즈 후 사용을 위한 변화 입니다. 
    
    ## Test Status
    * `ncc build index.js`

--- .github/actions/setup-mortar/dist/index.js
+++ .github/actions/setup-mortar/dist/index.js
@@ -13,7 +13,7 @@ async function setup() {
     try {
         // Download the specific version of the tool, e.g. as a tarball/zipball
         // TODO: https://ohouse.atlassian.net/browse/COREPL-1482 get latest version
-        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.1.6/mortar_Linux_x86_64.tar.gz');
+        const pathToTarball = await tc.downloadTool('https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/1.6.2/mortar_Linux_x86_64.tar.gz');
 
         // Extract the tarball onto the runner
         const pathToCLI = await tc.extractTar(pathToTarball);
